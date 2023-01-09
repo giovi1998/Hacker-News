2586c6c06a27146c66f98b65396c2bf2695c9670
Giovanni Cidu <you@example.com>
2023-01-10 00:16:59 +0100

    data.value

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -16,6 +16,7 @@ async function callLambdaFunction() {
     const response = await fetch("/.netlify/functions/lambda");
     const data = await response.json();
     console.log(data);
+    console.log(data.value);
   }
   
 

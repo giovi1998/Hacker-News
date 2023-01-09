7838429d5e39489819dbef286b317db036d32a6c
Giovanni Cidu <you@example.com>
2023-01-10 00:14:24 +0100

    json value

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -3,7 +3,7 @@ exports.handler = async function (event, context) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `Netlify is Good` },{value: value}),
+    body: JSON.stringify({value: value}),
     
   };  
 };
